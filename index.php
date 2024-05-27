2a172088df74ccfb681a88d82f56ab0c6156a63e
William <willspri@gmail.com>
2024-05-28 11:15:57 +1200

    added line breaks

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 //input
 $name = readline("Enter your name: ");
 echo "Hello " . $name . "\n";
-echo "Today's date is " . date("d/m/Y");
-echo "Today's date is " . date("Y.m.d");
-echo "Today's date is " . date("Y-m-d");
-echo "Today's date is " . date("l");
+echo "Today's date is " . date("d/m/Y") . "\n";
+echo "Today's date is " . date("Y.m.d") . "\n";
+echo "Today's date is " . date("Y-m-d") . "\n";
+echo "Today is " . date("l");
 ?>
 
