7c840b50e2b8fa722f7f2958f1861dd2395c8fb8
William <willspri@gmail.com>
2024-05-28 11:13:34 +1200

    added other formats

--- index.php
+++ index.php
@@ -3,5 +3,8 @@
 $name = readline("Enter your name: ");
 echo "Hello " . $name . "\n";
 echo "Today's date is " . date("d/m/Y");
+echo "Today's date is " . date("Y.m.d");
+echo "Today's date is " . date("Y-m-d");
+echo "Today's date is " . date("l");
 ?>
 
